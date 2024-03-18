10325bc67e9396c373107edf20ec1a77aa5fdf60
Crazy <58318108+CrazyCritic89@users.noreply.github.com>
2024-03-18 18:50:19 -0500

    A big rework, should be nicer

--- route.py
+++ route.py
@@ -8,9 +8,6 @@ source = input("Enter Source System: ")
 destin = input("Enter Destination System: ")
 capc = int(input("Enter Used Capacity [0]: ").strip() or "0")
 
-split = 490
-sizeinc = 5
-
 jumps = []
 apdat = []
 
@@ -36,112 +33,125 @@ if len(res1j) == 0:
 apdat = [res1j["name"], 0, 0]
 jumps.append(apdat)
 
-x1 = res1j["coords"]["x"]
-y1 = res1j["coords"]["y"]
-z1 = res1j["coords"]["z"]
+xv = res1j["coords"]["x"]
+yv = res1j["coords"]["y"]
+zv = res1j["coords"]["z"]
 
 if len(res2j) == 0:
     print("Received empty response. Destination system", '"'+destin+'"', "may be invalid.")
     sys.exit(0)
 
-x2 = res2j["coords"]["x"]
-y2 = res2j["coords"]["y"]
-z2 = res2j["coords"]["z"]
+xe = res2j["coords"]["x"]
+ye = res2j["coords"]["y"]
+ze = res2j["coords"]["z"]
 
-# Get distance between source and destination
+flag = True
 
-dist = math.dist([x1,y1,z1], [x2,y2,z2])
-
-# Get the differences of xyz between them
-xdif = x2 - x1
-ydif = y2 - y1
-zdif = z2 - z1
-
-ptpd = math.floor(dist/split)
-
-# Remove portition less than the split distance
-xdfp = xdif * split / dist
-ydfp = ydif * split / dist
-zdfp = zdif * split / dist
-
-# Set previous to source
-xprv = x1
-yprv = y1
-zprv = z1
+size = 60
+sinc = 10
 
 tfuel = 0
 
+acsys = []
+
 retrycount = 0
 
-i = 1
+xs = 0
+ys = 0
+zs = 0
 
-#split = 495
+print("Routing... 0%", end='')
 
-size = 1
+while True:
+    # Get distance between source and destination
+    dist = math.dist([xv,yv,zv], [xe,ye,ze])
 
-# Run through splits
-print("Routing...", end='')
-if ptpd > 0:
-    while i < ptpd+1:
-        xcur = x1 + xdfp * (i - (size/split))
-        ycur = y1 + ydfp * (i - (size/split))
-        zcur = z1 + zdfp * (i - (size/split))
+    if dist <= 500:
+        print("Routing... 100%", end='')
+        break
 
-        data ={"x" : xcur,
-               "y" : ycur,
-               "z" : zcur,
-               "size" : size,
-               "showCoordinates" : "1"}
+    # Get the differences of xyz between them
+    xd = xe - xv
+    yd = ye - yv
+    zd = ze - zv
 
-        res = ses.get("https://www.edsm.net/api-v1/cube-systems", params=data)
-        resj = res.json()
+    # Get step
+    xp = xd * 500 / dist
+    yp = yd * 500 / dist
+    zp = zd * 500 / dist
 
-        if res.status_code == 429:
-            if retrycount == 10:
-                print("\nRetry count exceeded. Exiting...")
-                sys.exit(1)
-            print("\nToo many requests. Trying again in 30 seconds...")
-            time.sleep(30)
-            retrycount += 1
-            ses = requests.Session()
+    data ={"x" : xv+xp,
+           "y" : yv+yp,
+           "z" : zv+zp,
+           "size" : size,
+           "showCoordinates" : "1"}
+
+    res = ses.get("https://www.edsm.net/api-v1/cube-systems", params=data)
+    resj = res.json()
+
+    if res.status_code == 429:
+        if retrycount == 10:
+            print("\nRetry count exceeded. Exiting...")
+            sys.exit(1)
+        print("\nToo many requests. Trying again in 30 seconds...")
+        time.sleep(30)
+        retrycount += 1
+        ses = requests.Session()
+        continue
+
+    if len(resj) == 0:
+        size += sinc
+        continue
+    else:
+        hold = 0
+        holc = 0
+        holi = -1
+        tmpi = 0
+        for i in resj:
+            xc = i["coords"]["x"]
+            yc = i["coords"]["y"]
+            zc = i["coords"]["z"]
+            jc = math.dist([xv,yv,zv],[xc,yc,zc])
+            dc = math.dist([xv+xp,yv+yp,zv+zp],[xc,yc,zc])
+            # Find longest distance from prev but shortest distance to split
+            if jc > hold:
+                if jc <= 500:
+                    if dc > holc:
+                        hold = jc
+                        holc = dc
+                        holi = tmpi
+            tmpi += 1
+        if holi == -1 or hold > 500:
+            size += sinc
             continue
-        
-        if len(resj) == 0:
-            size += sizeinc
-            continue
 
-        xsys = resj[0]["coords"]["x"]
-        ysys = resj[0]["coords"]["y"]
-        zsys = resj[0]["coords"]["z"]
+    xs = resj[holi]["coords"]["x"]
+    ys = resj[holi]["coords"]["y"]
+    zs = resj[holi]["coords"]["z"]
 
-        jdist = math.dist([xprv,yprv,zprv],[xsys,ysys,zsys])
+    jdist = math.dist([xv,yv,zv],[xs,ys,zs])
 
-        fuel = math.ceil(5+(jdist * (capc + 25000) / 200000))
-
-        apdat = [resj[0]["name"], jdist, fuel]
-        jumps.append(apdat)
-
-        tfuel = tfuel + fuel
-
-        xprv = xsys
-        yprv = ysys
-        zprv = zsys
-
-        size = 1
-
-        print("\rRouting...",str(round(i/(ptpd)*100))+"%", end='')
-        sys.stdout.flush()
-        
-        i += 1
-
-    jdist = math.dist([xsys,ysys,zsys],[x2,y2,z2])
     fuel = math.ceil(5+(jdist * (capc + 25000) / 200000))
-    tfuel = tfuel + fuel
-    apdat = [res2j["name"], jdist, fuel]
-else:
-    fuel = math.ceil(5+(dist * (capc + 25000) / 200000))
-    tfuel = fuel
-    apdat = [res2j["name"], dist, fuel]
+
+    apdat = [resj[holi]["name"], jdist, fuel]
+    jumps.append(apdat)
+
+    tfuel += fuel
+
+    xv = xs
+    yv = ys
+    zv = zs
+
+    size = 1
+
+    acsys = []
+
+    print("\rRouting...",str(round(xs/xe*100))+"%", end='')
+    
+jdist = math.dist([xs,ys,zs],[xe,ye,ze])
+fuel = math.ceil(5+(jdist * (capc + 25000) / 200000))
+tfuel = tfuel + fuel
+apdat = [res2j["name"], jdist, fuel]
 
 jumps.append(apdat)
 
@@ -158,5 +168,5 @@ print("\n"+"System".ljust(fitto+2)+"Jump Dist   Fuel")
 for i in jumps:
     print(str(i[0]).ljust(fitto+2)+str(round(i[1],2)).ljust(12)+str(i[2]))
 
-print("Total Jumps:", ptpd+1)
+print("Total Jumps:", len(jumps)-1)
 print("Total Fuel: ", tfuel)
